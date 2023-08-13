7b6f89daa61ec1a5ae56f71ae5be99b9d151616b
LosVocelos <janocelik@gmail.com>
2023-08-13 10:16:40 +0200

    Almost done...

--- src/parse.js
+++ src/parse.js
@@ -1,28 +1,41 @@
 const puppeteer = require('puppeteer');
+var fs = require('fs');
+
 var json = {};
 
 async function scrape() {
     const browser = await puppeteer.launch({});
     const page = await browser.newPage();
 
-    await page.goto('https://sortiment.makro.cz/cs/catalog/category/action/?producers=7430&?view_mode=bal&?view_price=s');
+    await page.goto('https://sortiment.makro.cz/cs/catalog/category/action/?producers=7430&view_mode=bal&view_price=s');
     const list = await page.$$("form.multiprodsubmitform > div.mo-products > div.product");
 
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < list.length; i++) {
         const name = await list[i].$eval("h3.product-title", i => i.innerText);
-        console.log(name);
+        json[name] = {};
 
         const pr_row = await list[i].$$("div.product-price-row");
 
         for (let j = 0; j < pr_row.length; j++) {
             const pr_lab = await pr_row[j].$eval("div.product-price-label", j => j.innerText);
             const pr_val = await pr_row[j].$eval("div.product-price-value", j => j.innerText);
-            console.log(pr_lab, pr_val);
+            json[name][pr_lab] = pr_val;
         }
         
     }
-        
     browser.close();
+
+    fs.writeFile('./akce.json', JSON.stringify(json, null, 4), function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("JSON saved to akce.json");
+        }
+    });
+}
+
+async function callback() {
+    console.log("done");
 }
 
 scrape();
